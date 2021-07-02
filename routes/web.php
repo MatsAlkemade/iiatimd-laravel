6424197a5922ca3dcd1ccc0ad87f3e5ed3b3aaec
MatsAlkemade <matsalkemade@gmail.com>
2021-07-02 18:52:14 +0200

    cocktail page added

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cocktails', function () {
+    $cocktails = \App\Models\Cocktail::all();
+
+    return $cocktails;
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
