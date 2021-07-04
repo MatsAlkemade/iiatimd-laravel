22f62d20b94ba7be3c153fe563a4b9f179ac5a92
MatsAlkemade <matsalkemade@gmail.com>
2021-07-05 00:10:07 +0200

    controller fix

--- app/Http/Controllers/Api/IngredientsController.php
+++ app/Http/Controllers/Api/IngredientsController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class IngredientsController extends Controller
 {
-    public function create(Request $request){
+    public function store(Request $request){
 
         $ingredient = new Ingredient;
         $ingredient->name = $request->name;
@@ -22,7 +22,7 @@ class IngredientsController extends Controller
         ]);
     }
 
-    public function delete(Request $request){
+    public function destroy(Request $request){
         $ingredient = Ingredient::find($request->id);
         $ingredient->delete();
         return response()->json([
