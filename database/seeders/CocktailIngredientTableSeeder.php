e7079394868d38a0376841856b2250a19566230e
MatsAlkemade <matsalkemade@gmail.com>
2021-07-05 20:53:13 +0200

    koppeltabel weg

--- database/seeders/CocktailIngredientTableSeeder.php
+++ database/seeders/CocktailIngredientTableSeeder.php
@@ -14,22 +14,22 @@ class CocktailIngredientTableSeeder extends Seeder
      */
     public function run()
     {
-        $mojito_ingredient_ids = [1, 2, 3, 4, 5, 6];
+        // $mojito_ingredient_ids = [1, 2, 3, 4, 5, 6];
 
-        foreach($mojito_ingredient_ids as $mojito_ingredient_id){
-            DB::table('cocktail_ingredient')->insert([
-                'cocktail_id' => 1,
-                'ingredient_id' => $mojito_ingredient_id,
-            ]);
-        }
+        // foreach($mojito_ingredient_ids as $mojito_ingredient_id){
+        //     DB::table('cocktail_ingredient')->insert([
+        //         'cocktail_id' => 1,
+        //         'ingredient_id' => $mojito_ingredient_id,
+        //     ]);
+        // }
 
-        $pina_colada_ingredient_ids = [1, 7, 8, 9];
+        // $pina_colada_ingredient_ids = [1, 7, 8, 9];
 
-        foreach($pina_colada_ingredient_ids as $pina_colada_ingredient_id){
-            DB::table('cocktail_ingredient')->insert([
-                'cocktail_id' => 2,
-                'ingredient_id' => $pina_colada_ingredient_id,
-            ]);
-        }
+        // foreach($pina_colada_ingredient_ids as $pina_colada_ingredient_id){
+        //     DB::table('cocktail_ingredient')->insert([
+        //         'cocktail_id' => 2,
+        //         'ingredient_id' => $pina_colada_ingredient_id,
+        //     ]);
+        // }
     }
 }
